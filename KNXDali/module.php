17af30c6137bfe10b8da096f31b8c691e6e60cb1
mannkar <forum@hofmann-kontakt.de>
2023-01-07 19:26:26 +0100

    upd trigger

--- KNXDali/module.php
+++ KNXDali/module.php
@@ -21,6 +21,29 @@ class KNXDali extends IPSModule {
     public function ApplyChanges() {
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
+
+        //***********************************************************
+        //Create our trigger
+        $_vii = 1;
+        //foreach     (PrimTrigger as &SourceVariable){
+
+
+			if(IPS_VariableExists($this->ReadPropertyInteger("PrimTrigger"))) {
+				$eid = @IPS_GetObjectIDByIdent("SourceTrigger".$_vii, $this->InstanceID);
+				if($eid === false) {
+					$eid = IPS_CreateEvent(0 /* Trigger */);
+					IPS_SetParent($eid, $this->InstanceID);
+					IPS_SetIdent($eid, "SourceTrigger".$_vii);
+					IPS_SetName($eid, "Trigger for #".$this->ReadPropertyInteger("PrimTrigger"));
+				}
+				IPS_SetEventTrigger($eid, 0, $this->ReadPropertyInteger("PrimTrigger"));
+				IPS_SetEventScript($eid, "SetValue(IPS_GetObjectIDByIdent(\"Value\", \$_IPS['TARGET']), UMR_Calculate(\$_IPS['TARGET'], \$_IPS['VALUE']));");
+				IPS_SetEventActive($eid, true);
+                $_vii++;
+			//}
+        }
+
+        //***********************************************************
     }
     /**
     * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
