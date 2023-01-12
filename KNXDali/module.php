69ac2071508381b9f31c983306d2de7b88c8dec9
mannkar <forum@hofmann-kontakt.de>
2023-01-12 08:27:00 +0100

    upd trig

--- KNXDali/module.php
+++ KNXDali/module.php
@@ -30,7 +30,8 @@ class KNXDali extends IPSModule {
                     IPS_SetParent($eid, $this->InstanceID);
 					IPS_SetIdent($eid, "PrimTrigger".$_vii);
                     $str = $variable;
-                    IPS_SetName($eid, "Trigger for #".$str ) ;
+                    IPS_SetName($eid, "Trigger for # ..." ) ;
+                    IPS_SetInfo($eid, $variable);
                 }
                 //IPS_SetEventTrigger($eid, 0, $this->ReadPropertyInteger("PrimTrigger"));
                 //IPS_SetEventScript($eid, "SetValue(IPS_GetObjectIDByIdent(\"Value\", \$_IPS['TARGET']), UMR_Calculate(\$_IPS['TARGET'], \$_IPS['VALUE']));");
