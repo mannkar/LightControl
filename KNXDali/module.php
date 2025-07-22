4bae01ccf87f55b1958b65003abd766476f9c2e3
mannkar <forum@hofmann-kontakt.de>
2025-07-22 16:12:03 +0200

    Active, Cleaning switch, external nominal value

--- KNXDali/module.php
+++ KNXDali/module.php
@@ -6,7 +6,7 @@ class KNXDali extends IPSModule {
         // Diese Zeile nicht löschen.
         parent::Create();
 
-        $this->RegisterPropertyInteger("PointOfLightDimm", 12345);
+        $this->RegisterPropertyInteger("PointOfLightDimm", 1);
         $this->RegisterPropertyInteger("PointOfLightOO", 0);
         $this->RegisterPropertyString("PrimTriggers",'[]');
         $this->RegisterPropertyString("SecTriggers", '[]');
@@ -14,12 +14,17 @@ class KNXDali extends IPSModule {
         $this->RegisterPropertyInteger("HolidayIndicatorID",0);
         $this->RegisterPropertyInteger("DayUsedWhenHoliday",0);
         $this->RegisterPropertyString("PrimDimVals", '[]');
-        
         $this->RegisterPropertyInteger("SecDimVal", 50);
-
+        $this->RegisterPropertyInteger("CleanDimVal", 80);
 
         $this->RegisterVariableBoolean('Active', $this->Translate('Active'), '~Switch');
         $this->EnableAction('Active');
+        $this->RegisterVariableBoolean('Cleaning', $this->Translate('Cleaning'), '~Switch');
+        $this->EnableAction('Cleaning');
+        $this->RegisterVariableFloat('Nominal', $this->Translate('Nominal value'), '', 20);
+        $this->EnableAction('Nominal');
+        $this->SetValue("Nominal", 20);
+
     }
 
     public function Destroy()
@@ -35,50 +40,45 @@ class KNXDali extends IPSModule {
         parent::ApplyChanges();
 
 
-        //Delete all references in order to re-add them
-		//Alle Referenzen löschen, um sie neu anzulegen
+        //Delete all references in order to re-add them - Alle Referenzen löschen, um sie neu anzulegen
         foreach ($this->GetReferenceList() as $referenceID) {
             $this->UnregisterReference($referenceID);
         }
         
-        //Delete all registrations in order to re-add them
-		//Alle Registrierungen löschen, um sie neu anzulegen
+        //Delete all registrations in order to re-add them - Alle Registrierungen löschen, um sie neu anzulegen
         foreach ($this->GetMessageList() as $senderID => $messages) {
             foreach ($messages as $message) {
                 $this->UnregisterMessage($senderID, $message);
             }
         }
 
-        //Register update messages and references
-		//Aktualisierungsmeldungen und Referenzen registrieren
+        //Register update messages and references - Aktualisierungsmeldungen und Referenzen registrieren
 
         if ($this->ReadPropertyInteger('PointOfLightDimm') != 0)
-            {
-                $this->RegisterReference($this->ReadPropertyInteger('PointOfLightDimm'));
-            }
+            { $this->RegisterReference($this->ReadPropertyInteger('PointOfLightDimm')); }
 
         if ($this->ReadPropertyInteger('PointOfLightOO') != 0)
-            {
-                $this->RegisterReference($this->ReadPropertyInteger('PointOfLightOO'));
-            }
+            { $this->RegisterReference($this->ReadPropertyInteger('PointOfLightOO')); }
+        
+        if ($this->ReadPropertyInteger('WeeklyTimeTableEventID') != 0)
+            { $this->RegisterReference($this->ReadPropertyInteger('WeeklyTimeTableEventID'));
+              $this->RegisterMessage($this->ReadPropertyInteger('WeeklyTimeTableEventID'), EM_UPDATE); }
+        
+        $this->RegisterMessage($this->GetIDForIdent('Cleaning'), VM_UPDATE);
 
         $inputTriggers = json_decode($this->ReadPropertyString('PrimTriggers'), true);
-        foreach ($inputTriggers as $inputTrigger) {
-            $triggerID = $inputTrigger['PrimTriggerIDs'];
+        foreach ($inputTriggers as $inputTrigger)
+            { $triggerID = $inputTrigger['PrimTriggerIDs'];
             $this->RegisterMessage($triggerID, VM_UPDATE);
-            $this->RegisterReference($triggerID);
-            }
+            $this->RegisterReference($triggerID); }
         
         $inputTriggers = json_decode($this->ReadPropertyString('SecTriggers'), true);
-        foreach ($inputTriggers as $inputTrigger) {
-            $triggerID = $inputTrigger['SecTriggerIDs'];
-            $this->RegisterMessage($triggerID, VM_UPDATE);
-            $this->RegisterReference($triggerID);
-            }
+        foreach ($inputTriggers as $inputTrigger)
+            { $triggerID = $inputTrigger['SecTriggerIDs'];
+            $this->RegisterMessage($triggerID, VM_UPDATE); 
+            $this->RegisterReference($triggerID); }
 
-
-        //Check status column for inputs
-		//Statusspalte für Eingänge prüfen
+        //Check status column for inputs - Statusspalte für Eingänge prüfen
         $inputTriggers = json_decode($this->ReadPropertyString('PrimTriggers'), true);
         $inputTriggerOkCount = 0;
         foreach ($inputTriggers as $inputTrigger) {
@@ -87,8 +87,7 @@ class KNXDali extends IPSModule {
             }
         }
 
-        //Check status column for inputs
-		//Statusspalte für Eingänge prüfen
+        //Check status column for inputs - Statusspalte für Eingänge prüfen
         $inputTriggers2 = json_decode($this->ReadPropertyString('SecTriggers'), true);
         $inputTriggerOkCount2 = 0;
         foreach ($inputTriggers2 as $inputTrigger2) {
@@ -98,16 +97,13 @@ class KNXDali extends IPSModule {
         }
     }
 
-
     public function GetConfigurationForm()
     {
         $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
-        //Add options to form
-		//Optionen zum Formular hinzufügen
+        //Add options to form - Optionen zum Formular hinzufügen
         $jsonForm = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
 
-        //Set status column for inputs
-		//Statusspalte für Eingänge setzen
+        //Set status column for inputs - Statusspalte für Eingänge setzen
         $inputTriggers = json_decode($this->ReadPropertyString('PrimTriggers'), true);
         foreach ($inputTriggers as $inputTrigger) {
             $jsonForm['elements'][2]['items'][0]['values'][] = [
@@ -120,14 +116,9 @@ class KNXDali extends IPSModule {
             $jsonForm['elements'][3]['items'][0]['values'][] = [
                 'Status' => $this->GetTriggerStatus($inputTrigger2['SecTriggerIDs'])
             ];
-        }
-        
-        
-
-        return json_encode($jsonForm);
-        
+        }    
+        return json_encode($jsonForm);   
     }
-
 
     private function GetTriggerStatus($triggerID)
     {
@@ -152,10 +143,17 @@ class KNXDali extends IPSModule {
     
     public function SetActive(bool $Active)
     {
-        
         //Modul aktivieren
         $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
         SetValue($this->GetIDForIdent('Active'), $Active);
+        return true;
+    }
+
+    public function SetCleaning(bool $Cleaning)
+    {
+        //Reinigung aktivieren
+        $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
+        SetValue($this->GetIDForIdent('Cleaning'), $Cleaning);
         return true;
     }
 
@@ -166,6 +164,9 @@ class KNXDali extends IPSModule {
             case 'Active':
                 $this->SetActive($Value);
                 break;
+            case 'Cleaning':
+                $this->SetCleaning($Value);
+                break;
 
             default:
                 throw new Exception('Invalid Ident');
@@ -173,64 +174,72 @@ class KNXDali extends IPSModule {
     }
 
     public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
-        $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
-        if ($Message == VM_UPDATE) {
-        //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
-        //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: "."Los ->");
-        //$this->WriteAttributeBoolean("Bulb", $this->ReadPropertyInteger("Trigger") );
-
-        //SetValueBoolean(46349, true);
-        $idSwitch = $this ->  ReadPropertyInteger("PointOfLightOO");
-        $idDimm = $this ->  ReadPropertyInteger("PointOfLightDimm");
-        //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message: ". $id);
-        //$value = GetValueBoolean ($this -> ReadPropertyInteger("Trigger")); 
-        $Level = $this -> TriggerStatus(); // off, low, high
-
-        $Message = "";
-        //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message: ".$idDimm. " " . $Message.$Level);
-        
-                switch ($Level)
+        //$this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
+        if (GetValue($this->GetIDForIdent('Active')))
         {
-            case "off":
-                //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with OFF Message: ".$idDimm. " " . $Message.$Level);
-                //SetValueInteger ($idDimm, 0);
-                RequestAction ($idDimm, 0);
-                break;
-            
-            case "low":
-                $BaseLevel = $this -> CalculateDimmLevel();
-                if ($BaseLevel > 0)
-                    {
-                    //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with LOW Message: ".$idDimm. " " . $Message.$Level);
-                    $this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with LOW Message: ".$idDimm. " " . $Message.$Level, 0);
-                    $Lower = $this -> ReadPropertyInteger("SecDimVal");
-                    //SetValueInteger ($idDimm, $BaseLevel/100*$Lower);
-                    RequestAction ($idDimm, $BaseLevel/100*$Lower);
+            if ($Message == EM_UPDATE)
+                {
+                    //$this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with Zeitupdate", 0);
+                    $BaseLevel = $this -> CalculateDimmLevel();
+                    //$this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with BaseLevel ".$BaseLevel, 0);
+                    SetValue($this->GetIDForIdent('Nominal'), $BaseLevel);
+                }
+
+            if ($Message == VM_UPDATE and $SenderID = $this->GetIDForIdent('Cleaning')) {
+                 // $this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with Cleaning Message: ", 0);
+                 $BaseLevel = $this -> ReadPropertyInteger("CleanDimVal");
+                 SetValue($this->GetIDForIdent('Nominal'), $BaseLevel);
+            }
+
+            if ($Message == VM_UPDATE) {
+                //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
+                //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: "."Los ->");
+                $idSwitch = $this ->  ReadPropertyInteger("PointOfLightOO");
+                $idDimm = $this ->  ReadPropertyInteger("PointOfLightDimm");
+                //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message: ". $id);
+                //$value = GetValueBoolean ($this -> ReadPropertyInteger("Trigger")); 
+                $Level = $this -> TriggerStatus(); // off, low, high
+                $Message = "";
+                //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message: ".$idDimm. " " . $Message.$Level);
+                
+                switch ($Level)
+                {
+                    case "off":
+                        //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with OFF Message: ".$idDimm. " " . $Message.$Level);
+                        RequestAction ($idDimm, 0);
+                        break;
+                    
+                    case "low":
+                        $BaseLevel = GetValueFloat($this->GetIDForIdent('Nominal'));
+                        if ($BaseLevel > 0)
+                            {
+                            //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with LOW Message: ".$idDimm. " " . $Message.$Level);
+                            $this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with LOW Message: ".$idDimm. " " . $Message.$Level, 0);
+                            $Lower = $this -> ReadPropertyInteger("SecDimVal");
+                            //SetValueInteger ($idDimm, $BaseLevel/100*$Lower);
+                            RequestAction ($idDimm, $BaseLevel/100*$Lower);
+                            }
+                        break;
+                    case "high":
+                        $BaseLevel = GetValueFloat($this->GetIDForIdent('Nominal'));
+                        if ($BaseLevel > 0)
+                            {
+                            //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with HIGH Message: ".$idDimm. " " . $Message.$Level);
+                            $this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with HIGH Message: ".$idDimm. " " . $Message.$Level, 0);
+                            //SetValueInteger ($idDimm, $BaseLevel);
+                            RequestAction ($idDimm, $BaseLevel);
+                            }
+                        break;
                     }
-                break;
-
-            case "high":
-                $BaseLevel = $this -> CalculateDimmLevel();
-                if ($BaseLevel > 0)
-                    {
-                    //IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with HIGH Message: ".$idDimm. " " . $Message.$Level);
-                    $this->SendDebug(__FUNCTION__, "Message from SenderID ".$SenderID." with HIGH Message: ".$idDimm. " " . $Message.$Level, 0);
-                    //SetValueInteger ($idDimm, $BaseLevel);
-                    RequestAction ($idDimm, $BaseLevel);
-                    }
-                break;
-
-
-        }
-        //SetValueBoolean($id , $value);
-        
+                //SetValueBoolean($id , $value);               
+                }
         }
     }    
 
     public function TimeTableEvent ()
     {
         //$this->RegisterPropertyInteger("WeeklyTimeTableEventID", 0);
-        $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
+        //$this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
         $TimeTable = $this -> ReadPropertyInteger ("WeeklyTimeTableEventID");
         $id = $TimeTable;
 
@@ -259,7 +268,7 @@ class KNXDali extends IPSModule {
 
     public function CalculateDimmLevel()
     {
-        $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
+        //$this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
         $DayTime = $this -> TimeTableEvent () -1; //
         //IPS_LogMessage("MessageSink", "DayTime Message: " . $DayTime);
         $this->SendDebug(__FUNCTION__, "DayTime Message: " . $DayTime, 0);
@@ -275,7 +284,7 @@ class KNXDali extends IPSModule {
 
     public function TriggerStatus ()
     {
-        $this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
+        //$this->SendDebug(__FUNCTION__,$_IPS['SENDER'] ,0);
         $PrimTriggerStatus = 0;
         $SecTriggerStatus = 0;
         $SumTriggerStatus = 0;
@@ -302,20 +311,20 @@ class KNXDali extends IPSModule {
         $SumTriggerStatus = $PrimTriggerStatus + $SecTriggerStatus;
         //IPS_LogMessage("MessageSink", "Message from SumTriggerStatus  "." with Message: ".$SumTriggerStatus);
         if ($SumTriggerStatus == 0)
-        {   $ReturnTirggerStatus = "off";
+        {   $ReturnTriggerStatus = "off";
             //IPS_LogMessage("MessageSink", "Message from Off Status  "." with Message: ".$ReturnTirggerStatus);
         }
         elseif ($SumTriggerStatus < 100)
-        {   $ReturnTirggerStatus = "low";
+        {   $ReturnTriggerStatus = "low";
             //IPS_LogMessage("MessageSink", "Message from LOW Status  "." with Message: ".$ReturnTirggerStatus);
         }
         elseif ($SumTriggerStatus >= 100)
-        {   $ReturnTirggerStatus = "high";
+        {   $ReturnTriggerStatus = "high";
             //IPS_LogMessage("MessageSink", "Message from hight Status  "." with Message: ".$ReturnTirggerStatus);
         }
 
         //IPS_LogMessage("MessageSink", "Message from ReturnTriggerStatus  "." with Message: ".$ReturnTirggerStatus);
-        return $ReturnTirggerStatus;
+        return $ReturnTriggerStatus;
 
     }
 }
